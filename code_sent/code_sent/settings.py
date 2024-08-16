f6dc9b1ea694c81a3988b567efcf74b69f7a447d
TmKUz0790 <104360495+TmKUz0790@users.noreply.github.com>
2024-08-16 15:28:59 +0500

    cool one
    cccc

--- code_sent/code_sent/settings.py
+++ code_sent/code_sent/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9=mn@y+bm^a(4df-m%d*g56fx$)upksl!mop6+spas5k+b310$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
